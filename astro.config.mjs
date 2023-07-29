083d3334631624ca85e72d57460bf941684afccf
Gravy59 <magiccoder59@gmail.com>
2023-07-28 18:14:21 -0600

    remove blog for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://gravy59.vercel.app/",
   integrations: [mdx(), sitemap(), tailwind()],
+  experimental: {
+    viewTransitions: true,
+  },
 });
--- src/content/blog/testing.md
+++
@@ -1,9 +0,0 @@
----
-title: Testing
-description: Testing
-pubDate: "Jul 27 2023"
----
-
-Testing
-
-## Testing
